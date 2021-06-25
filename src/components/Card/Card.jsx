8c722ae25aa0208cd6a1cf78feb096062a107f3d
dobkir <dobkir@gmail.com>
2021-06-25 17:13:54 +0400

    Added a method for adding selected items to the cart.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,32 @@
+import React from 'react';
 import styles from './Card.module.scss'
 
-function Card(props) {
+function Card({ title, imageUrl, price, onFavorite, onPlus }) {
+  const [isAdded, setIsAdded] = React.useState(false);
 
-  const onClickButton = () => {
-    alert(props.title);
+  const onClickPlus = () => {
+    onPlus({ title, imageUrl, price });
+    setIsAdded(!isAdded);
   };
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite}>
+      <div className={styles.favorite} onClick={onFavorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-      <h5>{props.title}</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>{props.price}</b>
+          <b>{price} руб.</b>
         </div>
-        <button className={styles.button} onClick={onClickButton}>
-          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-        </button>
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
+          alt="Plus button"
+        />
       </div>
     </div>
   );
