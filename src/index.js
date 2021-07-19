420ecf3ed4b726e746417829b66bcacc68c6ca3e
dobkir <dobkir@gmail.com>
2021-07-19 11:36:46 +0400

    mockAPI, search field, favorites, save to cart, React Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+
 import './index.scss';
 import 'macro-css';
 
@@ -8,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
